62640211a3c0467b1d3ec88fc6a8e759eec07400
eyk261 <eden.y.kifle@gmail.com>
2023-05-10 22:31:03 -0400

    completed

---
+++ Asset/script.js
@@ -0,0 +1,33 @@
+let myNumber = '12';
+
+if(typeof myNumber !=='number'){
+    console.log('This is not a number');
+}else if(myNumber%3 === 0){
+    console.log('Gen');
+}else if(myNumber % 5 === 0){
+    console.log("Buzz");
+}else if (myNumber % 3 === 0 && myNumber % 5 ===0){
+    console.log('GenBuzz');
+}else {
+    console.log(myNumber);
+}
+
+let items = shoes 
+switch (item){
+    case 'Shoes':
+        console.log('Shoes are 50$');
+        break;
+    case 'Jeans':
+        console.log('Jeans are $25');
+        case 'Hat':
+        console.log('Hats are 12$');
+        break;
+    case 'Socks':
+        console.log('Socks are $2');
+        break;
+    default:
+        console.log('Invalid Item');
+        break;
+}
+
+console.log(Math.floor(Math.random()* 51))
